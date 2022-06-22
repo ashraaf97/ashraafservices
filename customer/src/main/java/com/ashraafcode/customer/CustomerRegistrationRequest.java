e5aa078ca3eaa0430990ebee846909f1d40ae966
Muhammad Ashraaf bin Omar <58078254+ashraaf97@users.noreply.github.com>
2022-06-22 20:08:42 +0800

    Add model controller and Service

---
+++ customer/src/main/java/com/ashraafcode/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,12 @@
+package com.ashraafcode.customer;
+
+import lombok.Data;
+
+@Data
+public class CustomerRegistrationRequest {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
